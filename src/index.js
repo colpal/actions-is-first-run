15aa9404290073f3a7d45582457bb55fcf907534
Nicholas Farley <nicholasafarley@gmail.com>
2021-02-09 17:00:17 -0500

    Add implementation

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+(async () => {
+  const token = core.getInput('token', { required: true });
+
+  const owner = github.repository_owner;
+  const repo = github.event.repository.name;
+  const { run_id } = github;
+
+  const { created_at, updated_at } = await github
+    .getOctokit(token)
+    .actions
+    .getWorkflowRun({ owner, repo, run_id });
+
+  core.debug(JSON.stringify({
+    owner,
+    repo,
+    run_id,
+    created_at,
+    updated_at,
+  }, null, 2));
+
+  const isFirstRun = created_at === updated_at;
+  core.setOutput('is-first-run', isFirstRun);
+
+  const failFast = core.getInput('fail-fast', { required: false });
+  if (!isFirstRun && failFast === 'true') {
+    core.setFailed('This is a re-run, and "fail-fast" is set.');
+  }
+})();
